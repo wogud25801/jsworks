4b1f7d5532df2f1f57998d65a1e1b55bec2cee7b
woghd25801 <zx1148@naver.com>
2022-01-13 12:47:29 +0900

    day10 자바스크립트

---
+++ day10/JS/student.js
@@ -0,0 +1,11 @@
+//학생의 학년, 반, 이름 정보를 출력
+        var naming="전재홍"; //name 이라는 변수 선언
+        var grade = 2; // 선언과 동시에 초기화
+        //var class; //예약어는 변수로 사용할 수 없다.
+        var schoolClass = 3; //카멜표기 
+        
+        /*document.write(naming + '<br>');
+        document.write(grade + '<br>');
+        document.write(schoolClass + '<br>');*/
+        document.write('<p><span>' + naming + "</span>학생은" + grade 
+                       + "학년" + schoolClass + "반입니다.<p>")
